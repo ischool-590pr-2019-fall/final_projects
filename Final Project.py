f223f053dd3b94c55b9e192c9381df4d9b826500
Yuxuan Zhang <zhang489@iu.edu>
2019-12-06 18:07:42 -0600

    delete symbol function

--- Final Project.py
+++ Final Project.py
@@ -1,5 +1,13 @@
 from numba import jit
+import pandas as pd
+import numpy as n
 
 def import_data(file):
     with open (file, 'r') as f:
         print("Hello world!")
+
+
+def replace_symbol(dataset, col_name, symbol):
+    dataset[col_name] = dataset[col_name].str.replace(symbol, '')
+
+
